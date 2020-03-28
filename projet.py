45f0dc9b40dda531b83a47e4cd9b6a97bfa0ba6a
langevinjulien <54897903+langevinjulien@users.noreply.github.com>
2020-03-28 21:11:25 +0100

    Update projet.py
    Nom des superclasses, compilation et entraînement du modèle (non testé pour l'instant car trop long)

--- projet.py
+++ projet.py
@@ -2,7 +2,9 @@ from keras.datasets import cifar100                 #pour importer la base cifar
 import pandas                                       #bibliothèque utilisée pour la manipulation de données 
 from sklearn.preprocessing import LabelBinarizer    #pour utiliser la fonction LabelBinarizer (ligne 23)
 from keras.models import Sequential                 #ligne 31
-from keras.layers.core import Dense                 #ligne 33
+from keras.layers.core import Dense, Flatten        #ligne 33
+from keras.optimizers import SGD                    #Pour utiliser l'algorithme de descente du gradient
+import numpy as np 
 
 
 ##Chargement des données à partir de la bibliothèque Keras
@@ -19,6 +21,15 @@ split_f = cifar100.load_data(label_mode='fine')
 trainX_f = trainX_f.astype("float") / 255.0         #échantillon d'apprentissage
 testX_f = testX_f.astype("float") / 255.0           #échantillon test
 
+#Nom des superclasses
+Names = ["aquatic mammals", "fish", "flowers", "food containers",
+         "fruit and vegetables", "household electrical devices",
+         "household furniture", "insects", "large carnivores",
+         "large man-made outdoor things", "large natural outdoor scenes",
+         "large omnivires and herbivores","medium-sized mammals",
+         "non-insect invertebrates", "people", "reptiles", "small mammals",
+         "trees", "vehicules 1", "vehicles 2"]
+
 ##Transformer les noms de classes en vecteur
 #Les noms de classe sont représentés par des entiers (ex: classe 'cat' = 1)
 #On les transforme en vecteur (ex: classe 'cat' = [1 0 0 0 ...])
@@ -29,11 +40,49 @@ print(trainY_c)
 
 ##↨ Architecture du réseau 3072-1024-512-20
 model = Sequential()
-#Première couche: 3072 neurones, fonction d'activation: sigmoïde
-model.add(Dense(1024, input_shape=(3072,), activation="sigmoid"))
+#Première couche: 3072 neurones (32x32x3), fonction d'activation: sigmoïde
+model.add(Dense(1024, input_shape=(32,32,3), activation="sigmoid"))
 #Deuxième couche: 1024 neurones, fonction d'activation: sigmoïde
 model.add(Dense(512, activation="sigmoid"))
+model.add(Flatten())
 #Troisième couche: 512 neurones, fonction d'activation: softmax
 model.add(Dense(len(lb.classes_), activation="softmax"))         #ici len(lb.classes_)=20 car 20 superclasses
+
+#initialisation du taux d'apprentissage
+init_tapp = 0.01
+
+#Nombre d'itérations
+epochs = 30
+
+##Compilation du modèle
+opt = SGD(lr=init_tapp) #SGD = Stochastic Gradient Descent
+model.compile(loss="categorical_crossentropy", optimizer=opt, #fonction de perte
+              metrics=["accuracy"]) 
+
+##Entraînement du modèle (prend du temps /!\)
+H = model.fit(trainX_c, trainY_c, validation_data=(testX_c, testY_c), 
+              epochs=epochs, batch_size=32)
+
+##Evaluation du modèle
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
