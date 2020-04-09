d4f30215ed6d4ce85567ba8592dc1bff302fa723
CaneliaPichon <54897714+CaneliaPichon@users.noreply.github.com>
2020-04-09 19:39:06 +0200

    Nouveaux changements

--- projet.py
+++ projet.py
@@ -1,147 +1,148 @@
-import pandas                                       #bibliothèque utilisée pour la manipulation de données 
-from sklearn.preprocessing import LabelBinarizer    #pour utiliser la fonction LabelBinarizer (ligne 23)
+import numpy as np
+import pandas as pd                                 #Bibliothèque utilisée pour la manipulation de données
+import tensorflow as tf
+import imblearn                                     #Package pour l'undersampling, l'oversampling et le SMOTE 
+from imblearn.over_sampling import SMOTE
+from collections import Counter  
+from sklearn.preprocessing import LabelBinarizer    #Pour utiliser la fonction LabelBinarizer (ligne 23)
 from keras.models import Sequential                 
-from keras.layers.core import Dense, Flatten        
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D        
 from keras.optimizers import SGD                    #Pour utiliser l'algorithme de descente du gradient
-import numpy as np 
 from sklearn.metrics import classification_report   #Pour l'évaluation du modèle
+from sklearn.metrics import confusion_matrix        #Matrice de confusion
 import matplotlib.pyplot as plt                     #Pour les graphiques
 from keras.models import load_model                 #Pour utiliser un modèle sauvegardé
-import cv2      #Pour importer une image
+import cv2                                          #Pour importer une image
 
-##Importation des données
-#Fonction d'importation
+
+
+
+
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
-    
-#Importation des échantillons train et test
-train = unpickle('C:/Users/Utilisateur/Documents/M2_SEMESTRE_2/4_Python/Projet/cifar-100-python/train')
-test = unpickle('C:/Users/Utilisateur/Documents/M2_SEMESTRE_2/4_Python/Projet/cifar-100-python/test')
-train_X = train[b'data'] 
-train_Y = train[b'fine_labels']
-test_X = test[b'data']
-test_Y = test[b'fine_labels']
-
-#Importation des noms des classes
-meta = unpickle('C:/Users/Utilisateur/Documents/M2_SEMESTRE_2/4_Python/Projet/cifar-100-python/meta')
-fine_label_names = [t.decode('utf8') for t in meta[b'fine_label_names']]
-fine_label_names[0]
-print(fine_label_names.index('boy'))   #boy est la classe numéro 11
-print(fine_label_names.index('girl'))  #girl est la classe numéro 35
-print(fine_label_names.index('woman')) #woman est la classe numéro 98
-print(fine_label_names.index('man'))   #man est la classe numéro 46
-print(fine_label_names.index('baby'))  #baby est la classe numéro 2
+        cifar_dict = pickle.load(fo, encoding='bytes')
+    return cifar_dict
 
 
-#Changement du nom de la classe
-liste_classes = ["Humain","Non-humain"]
-#Dans l'échantillon d'apprentissage
+meta = unpickle('C:/Users/canou/Documents/Master 2/Semestre 2/Intro Python et ML/Projet/cifar-100-python/meta')
+train = unpickle('C:/Users/canou/Documents/Master 2/Semestre 2/Intro Python et ML/Projet/cifar-100-python/train')
+test = unpickle('C:/Users/canou/Documents/Master 2/Semestre 2/Intro Python et ML/Projet/cifar-100-python/test')
+
+fine_label_names=[t.decode('utf8') for t in meta[b'fine_label_names']]
+
+print(fine_label_names.index('boy'))
+print(fine_label_names.index('girl'))
+print(fine_label_names.index('man'))
+print(fine_label_names.index('woman'))
+print(fine_label_names.index('baby'))
+
+
+
+
+train_X = train[b"data"]
+train_Y = train[b"fine_labels"]
+
+test_X = test[b"data"]
+test_Y = test[b"fine_labels"]
+
+
+
+
+
+                                            #RECODAGE DES CLASSES
+#Pour l'échantillon d'apprentissage
+new_var=[]
 for i in range(50000):
-    if train_Y[i]==11:
-        train_Y[i]=0      #0: garçon
-    elif train_Y[i]==35:
-        train_Y[i]=0      #1: fille
-    elif train_Y[i]==98:
-        train_Y[i]=0      #2: femme
-    elif train_Y[i]==46:
-        train_Y[i]=0      #3: homme
-    elif train_Y[i]==2:
-        train_Y[i]=0     #4: bébé
+    if train_Y[i] in (11,35,46,98,2) :
+        nom=0
     else:
-        train_Y[i]=1      #5: non-humain
- 
-#Dans l'échantillon test       
+        nom=1
+    new_var.append(nom)  
+
+#Pour l'échantillon test
+new_test_Y=[]
 for i in range(10000):
-    if test_Y[i]==11:
-        test_Y[i]=0
-    elif test_Y[i]==35:
-        test_Y[i]=0
-    elif test_Y[i]==98:
-        test_Y[i]=0
-    elif test_Y[i]==46:
-        test_Y[i]=0
-    elif test_Y[i]==2:
-        test_Y[i]=0
+    if test_Y[i] in (11,35,46,98,2) :
+        nom=0
     else:
-        test_Y[i]=1
+        nom=1
+    new_test_Y.append(nom) 
+
+#Vérification
+new_var.count(0)
+new_var.count(1)
 
 
-#Récupérer la liste des éléments humains
-humains = [x for x in range(len(train_Y)) if train_Y[x] != 1]
-#Table des humains
-c=np.array(train_Y)
-d=np.array(train_X)
-train_Y_humains=c[humains]
-train_X_humains=d[humains]
 
-#Récupérer la liste des éléments humains non-humains
-non_humains=[i for i, n in enumerate(train_Y) if n == 1]
-train_Y_non_humains=c[non_humains]
-train_X_non_humains=d[non_humains]
 
-##Undersampling
-#Echantillon train de départ (50000 obs): 5% humains (2500 obs) / 95% de non-humains (47500 obs)
-idx = np.random.randint(47501, size=10000) 
-train_X_non_humains_und=train_X_non_humains[idx,:]  #Sur les explicatives
-train_Y_non_humains_und=train_Y_non_humains[idx]    #Sur la variable expliquée
 
-trainY=np.concatenate((train_Y_non_humains_und, train_Y_humains),axis=None)
-trainX=np.concatenate((np.array(train_X_non_humains_und), np.array(train_X_humains)), axis=0)
+#La base de données devient alors déséquilibrée puisqu'on a une prépondérance de non-humains. 
+#On choisit de faire de l'undersampling, de l'oversampling ou du smote.
+#Nous avons testé les différentes manières de gérer le déséquilibre et compte tenu des résultats,
+#nous avons choisi de faire du SMOTE.
+
+                                         #SMOTE
+smote = SMOTE(sampling_strategy='auto')
+X_smote, Y_smote = smote.fit_resample(train_X, new_var)
+#Résumé de la distribution des classes
+print(Counter(Y_smote)) 
+
+
+
+
 
 ##Transformation des noms de classes en vecteur
-#Les noms de classe sont représentés par des entiers (ex: classe 'garçon' = 1)
+#Les noms de classe sont représentés par des entiers (ex: classe 'humain' = 0)
 #On les transforme en vecteur (ex: classe 'cat' = [1 0 0 0 ...])
 lb = LabelBinarizer()
-trainY = lb.fit_transform(trainY)
-test_Y= lb.transform(test_Y)
-#trainY = trainY.reshape(1, -1)
-trainY2=np.array([[1,0] if l==1 else [0,1] for l in trainY])
-test_Y2=np.array([[1,0] if l==1 else [0,1] for l in test_Y])
+Y_smote = lb.fit_transform(Y_smote)
+new_test_Y= lb.transform(new_test_Y)
+
+train_Y=np.array([[1,0] if l==1 else [0,1] for l in Y_smote])
+test_Y=np.array([[1,0] if l==1 else [0,1] for l in new_test_Y])
+
+
+
+
 
 ##Normalisation des données
-trainX = trainX.reshape(12500, 3072).astype('float32') 
+train_X = X_smote.reshape(95000, 3072).astype('float32') 
 test_X = test_X.reshape(10000, 3072).astype('float32')
-trainX /= 255.0
+
+train_X /= 255.0
 test_X /= 255.0
 
 
 
-## Architecture du réseau 3072-1024-512-20
-model = Sequential()
-#Première couche: 3072 neurones (32x32x3), fonction d'activation: sigmoïde
-model.add(Dense(1024, input_shape=(3072,), activation="sigmoid"))
-#Deuxième couche: 1024 neurones, fonction d'activation: sigmoïde
-model.add(Dense(512, activation="sigmoid"))
-#model.add(Flatten())
-#Troisième couche: 512 neurones, fonction d'activation: softmax
-model.add(Dense(len(lb.classes_), activation="softmax"))
+#Arborescence du réseau de neurones
 
-#initialisation du taux d'apprentissage
-init_tapp = 0.01
+model = Sequential()
+model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(len(lb.classes_), activation='softmax'))
 
 #Nombre d'itérations
-epochs = 5
+epochs = 100
 
 ##Compilation du modèle
-opt = SGD(lr=init_tapp) #SGD = Stochastic Gradient Descent
-model.compile(loss="categorical_crossentropy", optimizer=opt, #fonction de perte
+model.compile(loss="binary_crossentropy", optimizer="adam", #fonction de perte
               metrics=["accuracy"]) 
 
 ##Entraînement du modèle 
-H = model.fit(trainX, trainY2, validation_data=(test_X, test_Y2), epochs=epochs, batch_size=64)
+H = model.fit(train_X, train_Y, epochs=epochs, batch_size=64, verbose=1, validation_split=0.2)
 #A essayer avec des batch size moins élevés
+score = model.evaluate(test_X, test_Y, verbose=0)
+print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
 
 
 ##Evaluation du modèle
+liste_classes = ["Non-humain","Humain"]
 print("Evaluation du modèle")
-predictions = model.predict(test_X, batch_size=32)
-print(classification_report(test_Y2.argmax(axis=1), predictions.argmax(axis=1), target_names=liste_classes))
+predictions = model.predict(test_X,batch_size=32)
+print(classification_report(test_Y.argmax(axis=1), predictions.argmax(axis=1), target_names=liste_classes))
 
-#Représentation graphique
+
 N = np.arange(0, epochs)
 plt.style.use("ggplot")
 plt.figure()
@@ -153,6 +154,14 @@ plt.title("Training Loss and Accuracy (Simple NN)")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend()
+
+
+
+
+
+#A REGARDER SI CA FONCTIONNE APRES : J'ai pas du tout touché à cette partie
+
+
 
 #Sauvegarde du modèle
 model.save("C:/Users/Utilisateur/Documents/M2_SEMESTRE_2/4_Python/Projet/projet.h5")
@@ -196,22 +205,3 @@ for i, v in enumerate(y):
 plt.title('Probablité prédite', family='serif', fontsize=15, style='italic', weight='bold', color='red', loc='center', rotation=0)
 plt.xlabel('Probabilité', fontsize=12, weight='bold', color='blue')
 plt.ylabel('Catégorie', fontsize=12, weight='bold', color='navy')
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
